5008fad63130bf0cac3a42f5c3fe31e7adc49699
Jocelyn Castro <jocelyn.cf@gmail.com>
2025-02-23 03:30:44 +0100

    cambio de archivo a donde corresponde

--- api/models/Clients.js
+++ api/models/Clients.js
@@ -1,11 +1,24 @@
 const mongoose = require('mongoose');
 
 const ClientSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  address: { type: String },
-  nif: { type: String },
-  email: { type: String, required: true, unique: true },
-  phone: { type: String },
-}, { timestamps: true });
+  name: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+  },
+  nif: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+  },
+});
 
 module.exports = mongoose.model('Client', ClientSchema);
